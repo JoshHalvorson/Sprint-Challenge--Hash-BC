e1d28f8eb81e48eed29735f31ec17a3a3549dd17
Josh Halvorson <Joshhalvorson16@gmail.com>
2019-10-04 11:38:53 -0700

    Add comments

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,10 +15,14 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
-
+    
+    # set up hash table with sources as keys, destinations as values
     for ticket in tickets:
         hash_table_insert(hashtable, ticket.source, ticket.destination)
+    # set first route to starting route (route with NONE source)
     route[0] = hash_table_retrieve(hashtable, 'NONE')
+    # Add routes to route list, skipping starting route
+    # and finding the ith location in the route by checking the hash table for the i-1th location
     for i in range(1, length):
         route[i] = hash_table_retrieve(hashtable, route[i - 1])
 
